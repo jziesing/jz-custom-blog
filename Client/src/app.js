9ab5b4ca2b8e693fa0d67b5cb33131431a450e06
jack <jack@jacks-MacBook-Air.local>
2021-10-06 00:54:41 -0700

    init commit

---
+++ Client/src/app.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
+import About from './about';
+import Login from './login';
+
+const App = () => (
+  <Router>
+    <div>
+      <header>
+        <Link to="/about">About</Link>
+        {' '}
+        <Link to="/">Login</Link>
+        {' '}
+      </header>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </main>
+    </div>
+  </Router>
+);
+
+export default App;
